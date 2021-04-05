6bcead76d5bf600db2aafed1d0c3bdbfc4d31d67
Allisson Santos <allisson.ferreira@outlook.com>
2021-04-04 22:06:17 -0300

    Create getProductIndexById method

--- src/util/format.ts
+++ src/util/format.ts
@@ -1,4 +1,10 @@
+import { ProductFormatted } from '../types';
+
 export const { format: formatPrice } = new Intl.NumberFormat('pt-br', {
   style: 'currency',
   currency: 'BRL',
 });
+
+export const getProductIndexById = function(id: number, products: ProductFormatted[]) {
+  return products.findIndex((product, index) => product.id === id );
+}
